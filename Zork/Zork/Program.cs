d0683d31255ebbfda0a2f7ba70866781b113847b
dprasad1027 <36938476+dprasad1027@users.noreply.github.com>
2019-09-11 14:43:53 -0400

    Update Program.cs

--- Zork/Zork/Program.cs
+++ Zork/Zork/Program.cs
@@ -20,16 +20,31 @@ namespace Zork
             Console.WriteLine("Welcome to Zork!");
 
             Commands command = Commands.UNKNOWN;
-            while(command != Commands.QUIT)
+            while (command != Commands.QUIT)
             {
                 Console.Write("> ");
                 command = ToCommand(Console.ReadLine().Trim());
 
-                string outputString = "";
+                string outputString;
                 switch (command)
                 {
+                    case Commands.QUIT:
+                        outputString = "Thank you for playing!";
+                        break;
+                    case Commands.LOOK:
+                        outputString = "This is an open field west of a white house, with a boarded front door. \nA rubber mat saying 'Welcome to Zork! lies by the door.";
+                        break;
+                    case Commands.NORTH:
+                    case Commands.SOUTH:
+                    case Commands.EAST:
+                    case Commands.WEST:
+                        outputString = $"You moved {command}.";
+                        break;
+
                     default:
-                    break;
+                        outputString = "Unknown command.";
+                        break;
+
                 }
 
                 Console.WriteLine(outputString);
