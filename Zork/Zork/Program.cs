56b6f29f5841260742b04e27d1914f55c0d1b4f5
dprasad1027 <36938476+dprasad1027@users.noreply.github.com>
2019-09-11 16:02:46 -0400

    7 - Expanding the Frontier - Review

--- Zork/Zork/Program.cs
+++ Zork/Zork/Program.cs
@@ -1,7 +1,21 @@
 ﻿using System;
-
+using System.Collections.Generic;
+using System.Diagnostics;
 namespace Zork
 {
+
+    public static class Assert
+    {
+        [Conditional("DEBUG")]
+        public static void IsTrue(bool expression, string message = null)
+        {
+            if (expression == false)
+            {
+                throw new Exception(message);
+            }
+        }
+    }
+
 
     enum Commands
     {
@@ -13,8 +27,16 @@ namespace Zork
         WEST,
         UNKNOWN
     }
+
     class Program
     {
+        private static string CurrentRoom
+        {
+            get
+            {
+                return Rooms[Location.Row, Location.Column];
+            }
+        }
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Zork!");
@@ -22,40 +44,84 @@ namespace Zork
             Commands command = Commands.UNKNOWN;
             while (command != Commands.QUIT)
             {
+                Console.WriteLine(CurrentRoom);
                 Console.Write("> ");
                 command = ToCommand(Console.ReadLine().Trim());
 
-                string outputString;
                 switch (command)
                 {
                     case Commands.QUIT:
-                        outputString = "Thank you for playing!";
+                        Console.WriteLine("Thank you for playing!");
                         break;
                     case Commands.LOOK:
-                        outputString = "This is an open field west of a white house, with a boarded front door. \nA rubber mat saying 'Welcome to Zork! lies by the door.";
+                        Console.WriteLine("A rubber mat saying 'Welcome to Zork! lies by the door.");
                         break;
                     case Commands.NORTH:
                     case Commands.SOUTH:
                     case Commands.EAST:
                     case Commands.WEST:
-                        outputString = $"You moved {command}.";
+                        if (Move(command) == false)
+                        {
+                            Console.WriteLine("The way is shut!");
+                        }
                         break;
 
                     default:
-                        outputString = "Unknown command.";
+                        Console.WriteLine("Unknown command.");
                         break;
 
                 }
 
-                Console.WriteLine(outputString);
+
             }
 
 
 
         }
 
-        private static Commands ToCommand(string commandString) => Enum.TryParse<Commands>(commandString, true, out Commands result) ? result : Commands.UNKNOWN;
+        private static bool Move(Commands command)
+        {
+            Assert.IsTrue(IsDirection(command), "Invalid direction.");
 
+            bool isValidMove = true;
+
+            switch (command)
+            {
+                case Commands.NORTH when Location.Row > 0:
+                    Location.Row--;
+                    break;
+                case Commands.SOUTH when Location.Row < Rooms.GetLength(0) - 1:
+                    Location.Row++;
+                    break;
+                case Commands.EAST when Location.Column < Rooms.GetLength(1) - 1:
+                    Location.Column++;
+                    break;
+                case Commands.WEST when Location.Column > 0:
+                    Location.Column--;
+                    break;
+
+                default:
+                    isValidMove = false;
+                    break;
+
+            }
+
+            return isValidMove;
+        }
+
+        private static Commands ToCommand(string commandString) => (Enum.TryParse(commandString, true, out Commands result) ? result : Commands.UNKNOWN);
+
+        private static bool IsDirection(Commands command) => Direction.Contains(command);
+
+        private static readonly List<Commands> Direction = new List<Commands>
+        {
+            Commands.NORTH,
+            Commands.SOUTH,
+            Commands.EAST,
+            Commands.WEST
+        };
+
+        private static (int Row, int Column) Location = (1, 1);
 
         private static readonly string[,] Rooms =
         {
