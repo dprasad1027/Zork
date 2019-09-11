207e59ee372a414e2ffdcb0d3b6752e79eab8b06
dprasad1027 <36938476+dprasad1027@users.noreply.github.com>
2019-09-11 14:14:18 -0400

    3 - Finding Your Direction

--- Zork/Zork/Program.cs
+++ Zork/Zork/Program.cs
@@ -3,7 +3,16 @@
 namespace Zork
 {
 
-
+    enum Commands
+    {
+        QUIT,
+        LOOK,
+        NORTH,
+        SOUTH,
+        EAST,
+        WEST,
+        UNKNOWN
+    }
     class Program
     {
         static void Main(string[] args)
@@ -11,22 +20,13 @@ namespace Zork
             Console.WriteLine("Welcome to Zork!");
 
             string inputString = Console.ReadLine();
-            inputString = inputString.ToUpper();
+            Commands command = ToCommand(inputString.Trim().ToUpper());
+            Console.WriteLine(command);
 
-            if(inputString == "QUIT")
-            {
-                Console.WriteLine("Thank you for playing.");
-            }else if(inputString == "LOOK")
-            {
-                Console.WriteLine("This is an open field west of a white house, with a boarded front door.\nA rubber mat saying 'Welcome to Zork!' lies by the door. ");
-            }
-            else
-            {
-                Console.WriteLine("Unrecognized command.");
-            }
-                              
-                  
-        } 
+        }
+
+        private static Commands ToCommand(string commandString) => Enum.TryParse(commandString, true, out Commands result) ? result : Commands.UNKNOWN;
+
 
     }
 }
