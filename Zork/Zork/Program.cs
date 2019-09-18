8c3d15b02112c1685050f3c2f3f5de96f999f655
dprasad1027 <36938476+dprasad1027@users.noreply.github.com>
2019-09-18 15:07:57 -0400

    10 - File Processing

--- Zork/Zork/Program.cs
+++ Zork/Zork/Program.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
 namespace Zork
 {
 
@@ -9,9 +12,14 @@ namespace Zork
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Zork!");
-            InitializeRoomDescriptions();
+           
             Room previousRoom = null;
-            
+
+            const string defaultRoomsFilename = "Rooms.txt";
+            string roomsFilename = (args.Length > 0 ? args[(int)CommandLineArguments.RoomsFilename] : defaultRoomsFilename);
+            InitializeRoomDescriptions(roomsFilename);
+
+
 
 
             Commands command = Commands.UNKNOWN;
@@ -56,6 +64,15 @@ namespace Zork
 
 
 
+        }
+
+        static Program()
+        {
+            RoomMap = new Dictionary<string, Room>();
+            foreach (Room room in Rooms)
+            {
+                RoomMap[room.Name] = room;
+            }
         }
 
  
@@ -111,37 +128,41 @@ namespace Zork
             { new Room("Dense Woods"), new Room("North of House"), new Room("Clearing") }
 
         };
-        private static void InitializeRoomDescriptions()
+
+
+        private static readonly Dictionary<string, Room> RoomMap;
+        private static void InitializeRoomDescriptions(string roomsFilename)
         {
-            var roomMap = new Dictionary<string, Room>();
-            foreach(Room room in Rooms)
+
+            const string fieldDelimiter = "##";
+            const int expectedFieldCount = 2;
+
+            var roomQuery = from line in File.ReadAllLines(roomsFilename)
+                            let fields = line.Split(fieldDelimiter)
+                            where fields.Length == expectedFieldCount
+                            select (Name: fields[(int)Fields.Name],
+                            Description: fields[(int)Fields.Description]);
+
+            foreach(var(Name,Description) in roomQuery)
             {
-                roomMap[room.Name] = room;
-            }
-
-            roomMap["Rocky Trail"].Description = "You are on rock-strewn trail.";
-            roomMap["South of House"].Description = "You are facing the south side of a white house. There is no door here, and all the windows are barred.";
-            roomMap["Canyon View"].Description = "You are at the top of the Great Canyon on its south wall.";
-            roomMap["Forest"].Description = "This is a forest, with trees in all directions around you.";
-            roomMap["West of House"].Description = "This is an open field west of a white house, with a boarded front door.";
-            roomMap["Behind House"].Description = "You are behind the white house. In one corner of the house there is a small window which is slightly ajar."; 
-            roomMap["Dense Woods"].Description = "This is a dimly lit forest, with large trees all around. To the east, there appears to be sunlight.";
-            roomMap["North of House"].Description = "You are facing the north side of a white house. There is no door here, and all the windows ar barred.";
-            roomMap["Clearing"].Description = "You are in a clearing, with a forest surrounding you on the west and south.";
 
 
-            //Rooms[0, 0].Description = "You are on a rock-strewn trail.";
-            //Rooms[0, 1].Description = "You are facing the south side of a white house. There is no door here, and all the windows are barred.";
-            //Rooms[0, 2].Description = "You are at the top of the Great Canyon on its south wall.";
+                RoomMap[Name].Description = Description;
 
-            //Rooms[1, 0].Description = "This is a forest, with trees in all directions around you.";
-            //Rooms[1, 1].Description = "This is an open field west of a white house, with a boarded front door.";
-            //Rooms[1, 2].Description = "You are behind the white house. In one corner of the house there is a small window which is slightly ajar.";
+            }                
 
-            //Rooms[2, 0].Description = "This is a dimly lit forest, with large trees all around. To the east, there appears to be sunlight.";
-            //Rooms[2, 1].Description = "You are facing the north side of a white house. There is no door here, and all the windows ar barred.";
-            //Rooms[2, 2].Description = "You are in a clearing, with a forest surrounding you on the west and south.";
 
+        }
+
+        private enum Fields
+        {
+            Name = 0,
+            Description
+        }
+
+        private enum CommandLineArguments
+        {
+            RoomsFilename = 0
         }
 
         public static Room CurrentRoom
