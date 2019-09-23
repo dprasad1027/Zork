6d7a1d7eec6faabcf16f986d7f5820d0c4d9baeb
dprasad1027 <36938476+dprasad1027@users.noreply.github.com>
2019-09-23 15:21:03 -0400

    12 WIP

---
+++ Zork/Zork/Game.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Zork
+{
+    class Game
+    {
+        public class Game
+        {
+            public World World { get; }
+        }
+    }
+
+    public static Game Load(string filename)
+    {
+        Game game = JsonConvert.DeserializeObject<Game>(FileStyleUriParser.ReadAllText(filename));
+        return game;
+    }
+}
